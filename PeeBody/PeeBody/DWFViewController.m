2388a4df4022171239446ea7e3a3891485958af4
Anthony Fennell <anthony@outsidesource.com>
2015-08-19 15:09:45 -0400

    DWFViewController: SlidersMoved only changes the respective value changed. Change of emitter cell selection will show its parameter values on sliders

--- PeeBody/PeeBody/DWFViewController.m
+++ PeeBody/PeeBody/DWFViewController.m
@@ -23,11 +23,6 @@
 @property (weak, nonatomic) IBOutlet UISlider *xAccelSlider;
 @property (weak, nonatomic) IBOutlet UISlider *zAccelSlider;
 
-
-
-
-
-
 @end
 
 @implementation DWFViewController
@@ -45,63 +40,69 @@
 
     UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, width, height / 2.0)];
     label.attributedText = [[NSAttributedString alloc] initWithString:@"Spin"
-                                                           attributes:@{NSFontAttributeName: [UIFont fontWithName:@"HelveticaNeue" size:10]}];
+                                                           attributes:@{NSFontAttributeName: [UIFont fontWithName:@"HelveticaNeue" size:12]}];
     [self.spinSlider addSubview:label];
     
     label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, width, height / 2.0)];
     label.attributedText = [[NSAttributedString alloc] initWithString:@"y Acceleration"
-                                                           attributes:@{NSFontAttributeName: [UIFont fontWithName:@"HelveticaNeue" size:10]}];
+                                                           attributes:@{NSFontAttributeName: [UIFont fontWithName:@"HelveticaNeue" size:12]}];
     [self.yAccelSlider addSubview:label];
     
     label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, width, height / 2.0)];
     label.attributedText = [[NSAttributedString alloc] initWithString:@"emission Range * pi/4"
-                                                           attributes:@{NSFontAttributeName: [UIFont fontWithName:@"HelveticaNeue" size:10]}];
+                                                           attributes:@{NSFontAttributeName: [UIFont fontWithName:@"HelveticaNeue" size:12]}];
     [self.emissionSlider addSubview:label];
     
     label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, width, height / 2.0)];
     label.attributedText = [[NSAttributedString alloc] initWithString:@"velocity"
-                                                           attributes:@{NSFontAttributeName: [UIFont fontWithName:@"HelveticaNeue" size:10]}];
+                                                           attributes:@{NSFontAttributeName: [UIFont fontWithName:@"HelveticaNeue" size:12]}];
     [self.velocitySlider addSubview:label];
     
     label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, width, height / 2.0)];
     label.attributedText = [[NSAttributedString alloc] initWithString:@"birth rate"
-                                                           attributes:@{NSFontAttributeName: [UIFont fontWithName:@"HelveticaNeue" size:10]}];
+                                                           attributes:@{NSFontAttributeName: [UIFont fontWithName:@"HelveticaNeue" size:12]}];
     [self.birthRateSlider addSubview:label];
     
     label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, width, height / 2.0)];
     label.attributedText = [[NSAttributedString alloc] initWithString:@"scaleSpeed"
-                                                           attributes:@{NSFontAttributeName: [UIFont fontWithName:@"HelveticaNeue" size:10]}];
+                                                           attributes:@{NSFontAttributeName: [UIFont fontWithName:@"HelveticaNeue" size:12]}];
     [self.scaleSpeedSlider addSubview:label];
     
     label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, width, height / 2.0)];
     label.attributedText = [[NSAttributedString alloc] initWithString:@"lifetime"
-                                                           attributes:@{NSFontAttributeName: [UIFont fontWithName:@"HelveticaNeue" size:10]}];
+                                                           attributes:@{NSFontAttributeName: [UIFont fontWithName:@"HelveticaNeue" size:12]}];
     [self.lifetimeSlider addSubview:label];
 
     label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, width, height / 2.0)];
     label.attributedText = [[NSAttributedString alloc] initWithString:@"velocity Range"
-                                                           attributes:@{NSFontAttributeName: [UIFont fontWithName:@"HelveticaNeue" size:10]}];
+                                                           attributes:@{NSFontAttributeName: [UIFont fontWithName:@"HelveticaNeue" size:12]}];
     [self.velocityRangeSlider addSubview:label];
 
     label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, width, height / 2.0)];
     label.attributedText = [[NSAttributedString alloc] initWithString:@"scale"
-                                                           attributes:@{NSFontAttributeName: [UIFont fontWithName:@"HelveticaNeue" size:10]}];
+                                                           attributes:@{NSFontAttributeName: [UIFont fontWithName:@"HelveticaNeue" size:12]}];
     [self.scaleSlider addSubview:label];
 
     label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, width, height / 2.0)];
     label.attributedText = [[NSAttributedString alloc] initWithString:@"lifetime range"
-                                                           attributes:@{NSFontAttributeName: [UIFont fontWithName:@"HelveticaNeue" size:10]}];
+                                                           attributes:@{NSFontAttributeName: [UIFont fontWithName:@"HelveticaNeue" size:12]}];
     [self.lifetimeRangeSlider addSubview:label];
     
     label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, width, height / 2.0)];
     label.attributedText = [[NSAttributedString alloc] initWithString:@"x Acceleration"
-                                                           attributes:@{NSFontAttributeName: [UIFont fontWithName:@"HelveticaNeue" size:10]}];
+                                                           attributes:@{NSFontAttributeName: [UIFont fontWithName:@"HelveticaNeue" size:12]}];
     [self.xAccelSlider addSubview:label];
 
     label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, width, height / 2.0)];
     label.attributedText = [[NSAttributedString alloc] initWithString:@"z Acceleration"
-                                                           attributes:@{NSFontAttributeName: [UIFont fontWithName:@"HelveticaNeue" size:10]}];
+                                                           attributes:@{NSFontAttributeName: [UIFont fontWithName:@"HelveticaNeue" size:12]}];
     [self.zAccelSlider addSubview:label];
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    [self setSliderValuesFromEmitterCell];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -138,18 +139,33 @@
 
 - (IBAction)slidersMoved:(id)sender
 {
-    [self.fireView setSpin:self.spinSlider.value];
-    [self.fireView setYAccel:self.yAccelSlider.value];
-    [self.fireView setEmissionRange:self.emissionSlider.value];
-    [self.fireView setVelocity:self.velocitySlider.value];
-    [self.fireView setBirthRate:self.birthRateSlider.value];
-    [self.fireView setScaleSpeed:self.scaleSpeedSlider.value];
-    [self.fireView setLifetime:self.lifetimeSlider.value];
-    [self.fireView setVelocityRange:self.velocityRangeSlider.value];
-    [self.fireView setScale:self.scaleSlider.value];
-    [self.fireView setLifetimeRange:self.lifetimeRangeSlider.value];
-    [self.fireView setZAccel:self.zAccelSlider.value];
-    [self.fireView setXAccel:self.xAccelSlider.value];
+    UISlider *slider = sender;
+    if (slider == self.spinSlider) {
+        [self.fireView setSpin:self.spinSlider.value];
+    } else if (slider == self.yAccelSlider) {
+        [self.fireView setYAccel:self.yAccelSlider.value];
+    } else if (slider == self.emissionSlider) {
+        [self.fireView setEmissionRange:self.emissionSlider.value];
+    } else if (slider == self.velocitySlider) {
+        [self.fireView setVelocity:self.velocitySlider.value];
+    } else if (slider == self.birthRateSlider) {
+        [self.fireView setBirthRate:self.birthRateSlider.value];
+    } else if (slider == self.scaleSpeedSlider) {
+        [self.fireView setScaleSpeed:self.scaleSpeedSlider.value];
+    } else if (slider == self.lifetimeSlider) {
+        [self.fireView setLifetime:self.lifetimeSlider.value];
+    } else if (slider == self.velocityRangeSlider) {
+        [self.fireView setVelocityRange:self.velocityRangeSlider.value];
+    } else if (slider == self.scaleSlider) {
+        [self.fireView setScale:self.scaleSlider.value];
+    } else if (slider == self.lifetimeRangeSlider) {
+        [self.fireView setLifetimeRange:self.lifetimeRangeSlider.value];
+    } else if (slider == self.zAccelSlider) {
+        [self.fireView setZAccel:self.zAccelSlider.value];
+    } else if (slider == self.xAccelSlider) {
+        [self.fireView setXAccel:self.xAccelSlider.value];
+    }
+    
 }
 
 
@@ -157,10 +173,15 @@
 {
     UISegmentedControl *control = (UISegmentedControl *)sender;
     if (control.selectedSegmentIndex == 0) {
-        [self.fireView showFire];
+        [self.fireView selectEmitterCell:EmitCellSmoke];
+    } else if (control.selectedSegmentIndex == 1) {
+        [self.fireView selectEmitterCell:EmitCellTwo];
+    } else if (control.selectedSegmentIndex == 2) {
+        [self.fireView selectEmitterCell:EmitCellThree];
     } else {
-        [self.fireView hideFire];
+        [self.fireView selectEmitterCell:EmitCellFour];
     }
+    [self setSliderValuesFromEmitterCell];
 }
 
 - (IBAction)emitterModeChanged:(id)sender {
@@ -234,9 +255,53 @@
 }
 
 
+#pragma mark - Cell Values
+
+- (void)setSliderValuesFromEmitterCell
+{
+    CAEmitterCell *cell = self.fireView.selectedCell;
+    self.spinSlider.value           = cell.spin;
+    self.yAccelSlider.value         = cell.yAcceleration;
+    self.emissionSlider.value       = cell.emissionRange / M_PI_4;
+    self.velocitySlider.value       = cell.velocity;
+    self.birthRateSlider.value      = cell.birthRate;
+    self.scaleSpeedSlider.value     = cell.scaleSpeed;
+    self.lifetimeSlider.value       = cell.lifetime;
+    self.velocityRangeSlider.value  = cell.velocityRange;
+    self.scaleSlider.value          = cell.scale;
+    self.lifetimeRangeSlider.value  = cell.lifetimeRange;
+    self.xAccelSlider.value         = cell.xAcceleration;
+    self.zAccelSlider.value         = cell.zAcceleration;
+}
+
+
 
 @end
 
+
+
+
+
+
+
+
+
+
+
+
+
+//@property (weak, nonatomic) IBOutlet UISlider *spinSlider;
+//@property (weak, nonatomic) IBOutlet UISlider *yAccelSlider;
+//@property (weak, nonatomic) IBOutlet UISlider *emissionSlider;
+//@property (weak, nonatomic) IBOutlet UISlider *velocitySlider;
+//@property (weak, nonatomic) IBOutlet UISlider *birthRateSlider;
+//@property (weak, nonatomic) IBOutlet UISlider *scaleSpeedSlider;
+//@property (weak, nonatomic) IBOutlet UISlider *lifetimeSlider;
+//@property (weak, nonatomic) IBOutlet UISlider *velocityRangeSlider;
+//@property (weak, nonatomic) IBOutlet UISlider *scaleSlider;
+//@property (weak, nonatomic) IBOutlet UISlider *lifetimeRangeSlider;
+//@property (weak, nonatomic) IBOutlet UISlider *xAccelSlider;
+//@property (weak, nonatomic) IBOutlet UISlider *zAccelSlider;
 
 
 
